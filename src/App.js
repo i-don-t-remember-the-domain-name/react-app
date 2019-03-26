7eb920d8e89358d10ff1a2db88209dc4b4e3581a
Ivana Huckova <ivana.huckova@gmail.com>
2019-03-26 14:46:33 +0100

    Create boilerplate for headings

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react';
-import './App.css';
 
-class App extends Component {
+//Styling
+import './App.scss';
+
+//Import Components
+import MainHeading from './Components/MainHeading';
+import Search from './Components/Search';
+
+//Default export
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
   render() {
-    return <div className="App" />;
+    return (
+      <div className="app-container">
+        <MainHeading />
+        <Search />
+      </div>
+    );
   }
 }
-
-export default App;
---
+++ src/Components/MainHeading.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import styled from 'styled-components';
+
+//Styling
+const SDHeadingContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const SDMainHeading = styled.h1`
+  color: orange;
+  font-weight: light;
+  font-size: 1.4rem;
+  line-height: 1.5;
+`;
+
+const SDUnderHeading = styled.h1`
+  font-weight: light;
+  font-size: 1rem;
+  line-height: 1.5;
+`;
+
+export default function MainHeading() {
+  return (
+    <SDHeadingContainer>
+      <SDMainHeading>Hacker Salt Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.</SDMainHeading>
+      <SDUnderHeading>
+        Hacker Salt Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+        dolore eu fugiat nulla pariatur.
+      </SDUnderHeading>
+    </SDHeadingContainer>
+  );
+}
