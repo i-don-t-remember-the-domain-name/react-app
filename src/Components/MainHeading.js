8bf4ec2cd567e40863d17196d7c593d6827e3b4d
Ivana Huckova <ivana.huckova@gmail.com>
2019-03-26 14:53:29 +0100

    Create boilerplate for searchbar

--- src/Components/MainHeading.js
+++ src/Components/MainHeading.js
@@ -1,6 +1,19 @@
 import React from 'react';
 import styled from 'styled-components';
 
+//Main Component For Export
+function MainHeading() {
+  return (
+    <SDHeadingContainer>
+      <SDMainHeading>Hacker Salt Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.</SDMainHeading>
+      <SDUnderHeading>
+        Hacker Salt Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+        dolore eu fugiat nulla pariatur.
+      </SDUnderHeading>
+    </SDHeadingContainer>
+  );
+}
+
 //Styling
 const SDHeadingContainer = styled.div`
   display: flex;
@@ -20,14 +33,5 @@ const SDUnderHeading = styled.h1`
   line-height: 1.5;
 `;
 
-export default function MainHeading() {
-  return (
-    <SDHeadingContainer>
-      <SDMainHeading>Hacker Salt Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.</SDMainHeading>
-      <SDUnderHeading>
-        Hacker Salt Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
-        dolore eu fugiat nulla pariatur.
-      </SDUnderHeading>
-    </SDHeadingContainer>
-  );
-}
+//Export
+export default MainHeading;
--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,12 +1,32 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
 
-export default class Search extends Component {
+//Main Component For Export
+class Search extends Component {
   render() {
     return (
-      <form>
-        <input placeholder="search hacker" />
-        <button>Submit</button>
-      </form>
+      <SDForm>
+        <SDInput placeholder="search hacker" />
+        <SDButton>Submit</SDButton>
+      </SDForm>
     );
   }
 }
+
+//Styling
+const SDForm = styled.form`
+  display: flex;
+`;
+
+const SDInput = styled.input`
+  width: 70%;
+  margin-right: 10px;
+  line-height: 2;
+`;
+
+const SDButton = styled.button`
+  width: 20%;
+`;
+
+//Export
+export default Search;
