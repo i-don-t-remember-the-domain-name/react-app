2c060c41fd191fa2cf55b7606649bb7e7334356b
Ivana Huckova <ivana.huckova@gmail.com>
2019-04-10 21:22:41 +0200

    Add link to parents comment in comment section

--- src/Components/UserProfile/CommentTable/CommentRow.js
+++ src/Components/UserProfile/CommentTable/CommentRow.js
@@ -7,6 +7,11 @@ export default function CommentRow(props) {
       <SDCommentRow>
         <SDRowSaltiestComment>{props.comment.cleaned_comment}</SDRowSaltiestComment>
         <SDRowSaltiness>{props.comment.comment_saltiness.toFixed(2)}</SDRowSaltiness>
+        <SDRowLink>
+          <a target="_blank" rel="noopener noreferrer" href={`https://news.ycombinator.com/item?id=${props.comment.parent_id}`}>
+            <i className="fas fa-arrow-right" />
+          </a>
+        </SDRowLink>
       </SDCommentRow>
     </div>
   );
@@ -29,6 +34,15 @@ const SDRowSaltiestComment = styled.div`
 `;
 
 const SDRowSaltiness = styled.div`
-  width: 5%;
+  width: 8%;
   text-align: center;
+`;
+
+const SDRowLink = styled.div`
+  width: 2%;
+  text-align: center;
+  cursor: pointer;
+  .fa-arrow-right {
+    color: #ff5d3e;
+  }
 `;
