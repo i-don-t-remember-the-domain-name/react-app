b3f7a0c5317bdfefa05bbc8181b43bb5ad239d0b
Ivana Huckova <ivana.huckova@gmail.com>
2019-04-11 20:36:48 +0200

    Create base for charts

---
+++ src/Components/Chart/BarChart.js
@@ -0,0 +1,31 @@
+import React, { PureComponent } from 'react';
+import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
+
+export default class Example extends PureComponent {
+  static jsfiddleUrl = 'https://jsfiddle.net/alidingling/30763kr7/';
+  commentsArray = Object.keys(this.props.hacker.monthly_plot).map(key => {
+    return { ...this.props.hacker.monthly_plot[key], key };
+  });
+  render() {
+    return (
+      <BarChart
+        width={1000}
+        height={300}
+        data={this.commentsArray}
+        margin={{
+          top: 5,
+          right: 30,
+          left: 20,
+          bottom: 5
+        }}>
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="key" />
+        <YAxis />
+        <Tooltip />
+        <Legend />
+        <Bar dataKey="c_h" fill="#74B816" />
+        <Bar dataKey="c_s" fill="#F4A000" />
+      </BarChart>
+    );
+  }
+}
---
+++ src/Components/Chart/LineChart.js
@@ -0,0 +1,34 @@
+import React, { PureComponent } from 'react';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, Bar } from 'recharts';
+
+export default class Example extends PureComponent {
+  static jsfiddleUrl = 'https://jsfiddle.net/alidingling/xqjtetw0/';
+
+  commentsArray = Object.keys(this.props.hacker.monthly_plot).map(key => {
+    return { ...this.props.hacker.monthly_plot[key], key };
+  });
+
+  render() {
+    console.log(this.commentsArray);
+    return (
+      <LineChart
+        width={1000}
+        height={300}
+        data={this.commentsArray}
+        margin={{
+          top: 5,
+          right: 30,
+          left: 20,
+          bottom: 5
+        }}>
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="key" />
+        <YAxis />
+        <Tooltip />
+        <Legend />
+        <Line type="monotone" dataKey="t_s" stroke="#F4A000" />
+        <Line type="monotone" dataKey="t_h" stroke="#74B816" />
+      </LineChart>
+    );
+  }
+}
