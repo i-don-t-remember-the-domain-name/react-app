91548db9a016410540359abfcd0476da86d33fe0
Britton Winterrose <BrittonWinterrose@users.noreply.github.com>
2019-04-14 22:42:07 -0700

    Update ExplanationSection.js

--- src/Components/HackerTopLists/ExplanationSection.js
+++ src/Components/HackerTopLists/ExplanationSection.js
@@ -10,6 +10,7 @@ export default function ExplanationSection(props) {
     <SDExplanationCard style={style}>
       <SDExplanationHeading style={style}>What does the score mean?</SDExplanationHeading>
       <div>We analyzed and scored the sentiment of 15,397,309 Hacker News comments and ranked users by their "saltiness" - a combination of subjective negativity. </div>
+      <div></div>
       <div>{props.explanation}</div>
     </SDExplanationCard>
   );
