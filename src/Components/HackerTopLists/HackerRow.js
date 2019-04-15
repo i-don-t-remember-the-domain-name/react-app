40c817beb31aed9ff7494d0f869f535c034cfada
Ivana Huckova <ivana.huckova@gmail.com>
2019-04-15 14:08:10 +0200

    Style main landing page

--- src/Components/HackerTopLists/HackerRow.js
+++ src/Components/HackerTopLists/HackerRow.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import styled from 'styled-components';
 
-//Import images
-import searchicon from '../../img/searchicon.png';
-
 //Export default component
 export default function HackerRow(props) {
   const rankKey = Object.keys(props.data).find(key => key.startsWith('rank'));
+  const style = {
+    color: props.color
+  };
 
   return (
     <SDHackerRow>
       <SDRowRank>{props.data[rankKey]}</SDRowRank>
-      <SDRowCommentor>{props.data.commentor}</SDRowCommentor>
+      <SDRowCommentor
+        style={style}
+        onClick={e => {
+          e.preventDefault();
+          props.searchHacker(props.data.commentor);
+        }}>
+        {props.data.commentor}
+      </SDRowCommentor>
       {/* 4 specific metrics below */}
       {props.data.sum_slt_s && <SDRowSpecificMetric>{props.data.sum_slt_s.toFixed(2)}</SDRowSpecificMetric>}
       {props.data.sum_slt_oall && <SDRowSpecificMetric>{props.data.sum_slt_oall.toFixed(2)}</SDRowSpecificMetric>}
@@ -19,13 +26,6 @@ export default function HackerRow(props) {
       {/* 4 specific metrics above */}
       {/* <SDRowSaltiestCommentHeading>Saltiest comment:</SDRowSaltiestCommentHeading> */}
       <SDRowSaltiestComment>{props.data.top_salty_comment[0].cleaned_comment}</SDRowSaltiestComment>
-      <SDRowLink
-        onClick={e => {
-          e.preventDefault();
-          props.searchHacker(props.data.commentor);
-        }}>
-        <img src={searchicon} alt="search icon" />
-      </SDRowLink>
     </SDHackerRow>
   );
 }
@@ -42,7 +42,7 @@ const SDHackerRow = styled.div`
   align-items: center;
   justify-content: space-between;
   font-size: 1rem;
-  cursor: auto;
+  cursor: default;
   @media (max-width: 900px) {
     font-size: 0.9rem;
   }
@@ -52,69 +52,38 @@ const SDHackerRow = styled.div`
 `;
 
 const SDRowRank = styled.div`
-  width: 5%;
+  width: 10%;
   @media (max-width: 600px) {
     flex-order: 1;
-    width: 20%;
+    width: 10%;
   }
 `;
 
 const SDRowCommentor = styled.div`
-  min-width: 16%;
+  min-width: 20%;
+  cursor: pointer;
   @media (max-width: 600px) {
     order: 2;
-    width: 40%;
+    width: 30%;
   }
 `;
 
 const SDRowSpecificMetric = styled.div`
-  width: 12%;
+  width: 10%;
   @media (max-width: 600px) {
     order: 3;
-    width: 25%;
+    width: 55%;
+    text-align: right;
   }
 `;
 
 const SDRowSaltiestComment = styled.div`
-  width: 54%;
+  width: 60%;
   @media (max-width: 600px) {
     width: 100%;
     padding-top: 10px;
     margin-top: 10px;
     order: 6;
     border-top: 1px solid #f8f9fa;
-  }
-`;
-
-// const SDRowSaltiestCommentHeading = styled.div`
-//   display: none;
-//   @media (max-width: 600px) {
-//     border-top: 1px solid #f8f9fa;
-//     display: flex;
-//     width: 100%;
-//     margin-top: 10px;
-//     padding-top: 10px;
-//     order: 5;
-//   }
-// `;
-
-const SDRowLink = styled.div`
-  width: 8%;
-  cursor: pointer;
-  text-align: center;
-  @media (max-width: 600px) {
-    order: 4;
-    width: 10%;
-  }
-  img {
-    width: 1rem;
-  }
-  img {
-    @media (max-width: 900px) {
-      width: 0.9rem;
-    }
-    @media (max-width: 800px) {
-      width: 0.8rem;
-    }
   }
 `;
