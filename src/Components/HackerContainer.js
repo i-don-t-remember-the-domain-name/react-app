fa7ae67e134b7f91a0c032f0275ce9256e6c7611
Ivana Huckova <ivana.huckova@gmail.com>
2019-04-27 12:31:30 +0200

    Create firebase fetch function replacing axios call

--- src/Components/HackerContainer.js
+++ src/Components/HackerContainer.js
@@ -1,59 +1,64 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
-import axios from 'axios';
+// import axios from 'axios';
 
 //Import Components
 import RoutesContainer from './RoutesContainer';
 import Footer from './Footer/Footer';
 
+//Import DB
+import db from '../fbConfig';
+
 //Default export
 function HackerContainer(props) {
   const searchHacker = hacker => {
     props.setLoading(true);
-    axios({
-      method: 'get',
-      url: `https://hacker-salt.herokuapp.com/api/hacker/${hacker}`,
-      timeout: 5000
-    })
-      .then(res => {
-        props.setError(false);
-        cleanPreviousHacker();
-        props.setHackerName(res.data.commentor);
-        props.setCountOfAllComments(res.data.cnt_cmnts_oall);
-        props.setCountOfSaltyComments(res.data.cnt_slt_s);
-        props.setRankAmountOfSaltiness(res.data.rank_lt_amt_slt);
-        props.setRankQuantityOfSaltiness(res.data.rank_lt_qty_sc);
-        props.setRankOverallSaltiness(res.data.rank_oall_slt);
+    db.collection('commentor_stats')
+      .where('commentor', '==', hacker)
+      .get()
+      .then(qs => {
+        qs.forEach(doc => {
+          console.log(doc.data());
+          props.setError(false);
+          cleanPreviousHacker();
+          props.setHackerName(doc.data().commentor);
+          props.setCountOfAllComments(doc.data().cnt_cmnts_oall);
+          props.setCountOfSaltyComments(doc.data().cnt_slt_s);
+          console.log(doc.data().rank_oall_slt);
+          !isNaN(doc.data().rank_lt_amt_slt) && props.setRankAmountOfSaltiness(doc.data().rank_lt_amt_slt);
+          !isNaN(doc.data().rank_lt_qty_sc) && props.setRankQuantityOfSaltiness(doc.data().rank_lt_qty_sc);
+          !isNaN(doc.data().rank_oall_slt) && props.setRankOverallSaltiness(doc.data().rank_oall_slt);
 
-        //If there is average saltiness, fix it to 2 decimal numbers
-        res.data.avg_slt_s && props.setAverageSaltiness(res.data.avg_slt_s.toFixed(2));
+          //If there is average saltiness, fix it to 2 decimal numbers
+          doc.data().avg_slt_s && props.setAverageSaltiness(doc.data().avg_slt_s.toFixed(2));
 
-        //If there is date, transform date to required format and set the DateOfFirstComment state
-        if (res.data.time_cmnt_fst.seconds) {
-          let dateInfo = new Date(res.data.time_cmnt_fst.seconds * 1000);
-          let date = `${dateInfo.getDate()}-${dateInfo.getMonth()}-${dateInfo.getFullYear()}`;
-          props.setDateOfFirstComment(date);
-        }
+          //If there is date, transform date to required format and set the DateOfFirstComment state
+          if (doc.data().time_cmnt_fst.seconds) {
+            let dateInfo = new Date(doc.data().time_cmnt_fst.seconds * 1000);
+            let date = `${dateInfo.getDate()}-${dateInfo.getMonth()}-${dateInfo.getFullYear()}`;
+            props.setDateOfFirstComment(date);
+          }
 
-        //Transform saltiest comments object to array and set the SaltiestComments state and sort them by the saltiness
-        if (res.data.top_cmnts_s) {
-          const saltiestCommentsArray = Object.keys(res.data.top_cmnts_s).map(key => {
-            return res.data.top_cmnts_s[key];
-          });
-          props.setSaltiestComments(
-            saltiestCommentsArray.sort(function(a, b) {
-              return a.comment_saltiness - b.comment_saltiness;
-            })
-          );
-        }
+          //Transform saltiest comments object to array and set the SaltiestComments state and sort them by the saltiness
+          if (doc.data().top_cmnts_s) {
+            const saltiestCommentsArray = Object.keys(doc.data().top_cmnts_s).map(key => {
+              return doc.data().top_cmnts_s[key];
+            });
+            props.setSaltiestComments(
+              saltiestCommentsArray.sort(function(a, b) {
+                return a.comment_saltiness - b.comment_saltiness;
+              })
+            );
+          }
 
-        //If monthly plot is there, transform monthly plot object to array and set the monthly Plot state
-        if (res.data.monthly_plot) {
-          const monthlyPlotArray = Object.keys(res.data.monthly_plot).map(key => {
-            return { ...res.data.monthly_plot[key], key };
-          });
-          props.setMonthlyPlot(monthlyPlotArray);
-        }
+          //If monthly plot is there, transform monthly plot object to array and set the monthly Plot state
+          if (doc.data().monthly_plot) {
+            const monthlyPlotArray = Object.keys(doc.data().monthly_plot).map(key => {
+              return { ...doc.data().monthly_plot[key], key };
+            });
+            props.setMonthlyPlot(monthlyPlotArray);
+          }
+        });
       })
       .then(() => props.setLoading(false))
       .then(() => {
