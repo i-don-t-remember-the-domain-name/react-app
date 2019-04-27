97960ae2cee4f01d748e97538cd3a079a44cd3a2
Ivana Huckova <ivana.huckova@gmail.com>
2019-04-27 11:55:16 +0200

    Add firebase config

---
+++ src/fbConfig.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase/app'
+var config = ;
+firebase.initializeApp(config);
+
+const firebase = require('firebase/app');
+require('firebase/firestore');
+
+//Firebase configuration
+firebase.initializeApp({
+    apiKey: "AIzaSyCAUlq3XK2lXeTFsQThdL4GyivWx-c5DCQ",
+    authDomain: "winterrose-nlp.firebaseapp.com",
+    databaseURL: "https://winterrose-nlp.firebaseio.com",
+    projectId: "winterrose-nlp",
+    storageBucket: "winterrose-nlp.appspot.com",
+    messagingSenderId: "468502233263"
+  });
+const db = firebase.firestore();
+
+export default db;
