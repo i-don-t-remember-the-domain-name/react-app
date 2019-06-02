35fd84c5135622b7d8137e3b5b50d69c7f404642
Ivana Huckova <ivana.huckova@gmail.com>
2019-06-02 21:50:05 +0200

    Style hacker's comment table

--- src/Components/HackerPage/CommentsTable/CommentTopRow.js
+++ src/Components/HackerPage/CommentsTable/CommentTopRow.js
@@ -7,72 +7,72 @@ import { dtGreen, dtDarkBlue, ltWhite, ltBlue } from '../../../colors.js';
 
 //Export default component
 export default function CommentTopRow(props) {
-    return (
-        <SDCommentRow>
-            <SDRowSaltiestComment>Comments</SDRowSaltiestComment>
-            <SDRowSaltiness>Score</SDRowSaltiness>
-            <SDRowLink>See on HN</SDRowLink>
-        </SDCommentRow>
-    );
+  return (
+    <SDCommentRow>
+      <SDRowSaltiestComment>Comments</SDRowSaltiestComment>
+      <SDRowSaltiness>Score</SDRowSaltiness>
+      <SDRowLink>See on HN</SDRowLink>
+    </SDCommentRow>
+  );
 }
 
 //Theming
 const headingColor = theme('mode', {
-    light: ltBlue,
-    dark: dtGreen
+  light: ltBlue,
+  dark: dtGreen
 });
 
 const backgroundColor = theme('mode', {
-    light: ltWhite,
-    dark: dtDarkBlue
+  light: ltWhite,
+  dark: dtDarkBlue
 });
 
 //Styled components
 const SDCommentRow = styled.div`
-    width: 100%;
-    min-height: 50px;
-    height: auto;
-    margin: 5px 0;
-    padding: 10px;
-    background-color: ${backgroundColor};
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    color: ${headingColor};
-    @media (max-width: 600px) {
-        font-size: 0.9rem;
-    }
+  /*themed*/
+  background-color: ${backgroundColor};
+  color: ${headingColor};
+  /*non-themed*/
+  width: 100%;
+  min-height: 50px;
+  height: auto;
+  margin: 5px 0;
+  padding: 10px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  @media (max-width: 600px) {
+    font-size: 0.9rem;
+  }
 `;
 
 const SDRowSaltiestComment = styled.div`
-    width: 78%;
-    padding-left: 5px;
-    font-weight: 600;
-    @media (max-width: 600px) {
-        width: 60%;
-    }
+  width: 78%;
+  padding-left: 5px;
+  font-weight: 600;
+  @media (max-width: 600px) {
+    width: 60%;
+  }
 `;
 
 const SDRowSaltiness = styled.div`
-    width: 10%;
-    color: #4c6cec;
-    font-weight: 600;
-    text-align: center;
-    padding-left: 20px;
-    @media (max-width: 600px) {
-        width: 15%;
-        padding-left: 0px;
-    }
+  width: 10%;
+  font-weight: 600;
+  text-align: center;
+  padding-left: 20px;
+  @media (max-width: 600px) {
+    width: 15%;
+    padding-left: 0px;
+  }
 `;
 
 const SDRowLink = styled.div`
-    width: 10%;
-    color: #4c6cec;
-    font-weight: 600;
+  width: 10%;
+  font-weight: 600;
+  text-align: center;
+  @media (max-width: 600px) {
+    width: 15%;
     text-align: center;
-
-    @media (max-width: 600px) {
-        width: 15%;
-        text-align: center;
-    }
+  }
 `;
