e68d0f91e03d5bb98c5816bf80f5a50416e8e73a
Ivana Huckova <ivana.huckova@gmail.com>
2019-06-02 18:22:00 +0200

    Style about us

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -2,35 +2,38 @@ import React from 'react';
 import styled from 'styled-components';
 import theme from 'styled-theming';
 
+//Import colors
+import { dtPlaceholder, ltPlaceholder } from '../../colors.js';
+
 //Export default component
 export default function Footer() {
-  return (
-    <SDFooter>
-      <div>
-        Created by <a href="https://twitter.com/ivanahuckova">Ivana</a> and{' '}
-        <a href="https://twitter.com/bwinterrose">Britton</a>. &copy; 2019
-      </div>
-    </SDFooter>
-  );
+    return (
+        <SDFooter>
+            <div>
+                Created by <a href="https://twitter.com/ivanahuckova">Ivana</a> and <a href="https://twitter.com/bwinterrose">Britton</a>. &copy; 2019
+            </div>
+        </SDFooter>
+    );
 }
 
 //Theming
 const textColor = theme('mode', {
-  light: 'gray',
-  dark: '#fff'
+    light: ltPlaceholder,
+    dark: dtPlaceholder
 });
 
 //Styled components
 const SDFooter = styled.footer`
-  box-sizing: border-box;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  color: gray;
-  font-size: 0.7rem;
-  height: 70px;
-  width: 100vw;
-  padding-bottom: 10px;
-  padding-top: 20px;
-  color: ${textColor};
+    /*themed*/
+    color: ${textColor};
+    /*non-themed*/
+    box-sizing: border-box;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 0.7rem;
+    height: 70px;
+    width: 100vw;
+    padding-bottom: 10px;
+    padding-top: 20px;
 `;
