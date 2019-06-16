28efd6721a20f76487def146fff9638d935a15cd
Ivana Huckova <ivana.huckova@gmail.com>
2019-06-16 19:18:32 +0200

    Style banner

--- src/Components/Searchbar/SearchBanner.js
+++ src/Components/Searchbar/SearchBanner.js
@@ -10,54 +10,57 @@ import { ltOrange, dtWhiteFont } from '../../colors.js';
 
 //Export default component
 export default function SearchBanner(props) {
-  return (
-    <SDSearchBanner>
-      <h1>find the most TOXIC HACKERS on internet</h1>
-      <SDSearchBarFull>
-        <SearchBar searchHacker={props.searchHacker} redirectToHackerProfilePage={props.redirectToHackerProfilePage} loading={props.loading} error={props.error} />
-      </SDSearchBarFull>
-    </SDSearchBanner>
-  );
+    return (
+        <SDSearchBanner>
+            <h1>find the most TOXIC HACKERS on internet</h1>
+            <SDSearchBarFull>
+                <SearchBar searchHacker={props.searchHacker} redirectToHackerProfilePage={props.redirectToHackerProfilePage} loading={props.loading} error={props.error} />
+            </SDSearchBarFull>
+        </SDSearchBanner>
+    );
 }
 
 //Theming
 const h1Color = theme('mode', {
-  light: ltOrange,
-  dark: dtWhiteFont
+    light: ltOrange,
+    dark: dtWhiteFont
 });
 
 //Styled components
 const SDSearchBanner = styled.div`
-  width: 100vw;
-  height: 50vh;
-  margin-top: 70px;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-  h1 {
-    /*themed*/
-    color: ${h1Color};
-    /*non-themed*/
-    font-family: 'Rubik', -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;
-    font-size: 2rem;
-    font-weight: 400;
-  }
+    width: 100vw;
+    height: 50vh;
+    margin-top: 70px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    @media (max-width: 1100px) {
+        padding: 0 10px;
+    }
+    h1 {
+        /*themed*/
+        color: ${h1Color};
+        /*non-themed*/
+        font-family: 'Rubik', -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;
+        font-size: 2rem;
+        font-weight: 400;
+    }
 `;
 
 const SDSearchBarFull = styled.div`
-  margin-top: 80px;
-  margin-bottom: 20px;
-  height: 40px;
-  width: 30%;
-  @media (max-width: 1100px) {
-    width: 40%;
-  }
-  @media (max-width: 900px) {
-    width: 50%;
-  }
-  @media (max-width: 600px) {
-    width: 80%;
-  }
+    margin-top: 80px;
+    margin-bottom: 20px;
+    height: 40px;
+    width: 30%;
+    @media (max-width: 1100px) {
+        width: 40%;
+    }
+    @media (max-width: 900px) {
+        width: 50%;
+    }
+    @media (max-width: 600px) {
+        width: 80%;
+    }
 `;
