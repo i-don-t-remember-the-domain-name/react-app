9abde085b0905361f945e4a4b811ce32fb768e47
Ivana Huckova <ivana.huckova@gmail.com>
2019-06-16 18:53:20 +0200

    Fix mobile responsivness

--- src/Components/WelcomeModal/WelcomeModal.js
+++ src/Components/WelcomeModal/WelcomeModal.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, Icon, Modal } from 'semantic-ui-react';
+import { Button, Modal } from 'semantic-ui-react';
 import styled from 'styled-components';
 
 export default function ModalBasicExample(props) {
@@ -13,37 +13,56 @@ export default function ModalBasicExample(props) {
         window.location.assign('https://www.google.com/search?q=puppies&source=lnms&tbm=isch&sa=X&ved=0ahUKEwipu-aoru7iAhVQIlAKHeDODxAQ_AUIECgB&biw=1920&bih=946');
     };
     return (
-        <Modal open={props.showModal} basic size="small">
-            <Modal.Content>
-                <SDModalContentContainer>
-                    <SDModalHeader>You are about to enter TOXIC HACKERS</SDModalHeader>
-                    <SDModalContent>The following website contains strong language which may be offensive to some people and inappropriate for children. You have been warned!</SDModalContent>
-                </SDModalContentContainer>
-            </Modal.Content>
-            <Modal.Actions>
-                <SDModalContainer>
-                    <Button className="closeButton" onClick={() => leavePage()}>
-                        <Icon name="close" /> I would rather see some puppies
-                    </Button>
-                    <Button className="proceedButton" onClick={e => onClose(e)}>
-                        <Icon name="checkmark" /> Proceed
-                    </Button>
-                </SDModalContainer>
-            </Modal.Actions>
+        <Modal open={props.showModal} basic size="large">
+            <SDModal>
+                <Modal.Content>
+                    <SDModalContentContainer>
+                        <SDModalHeader>You are about to enter TOXIC HACKERS</SDModalHeader>
+                        <SDModalContent>The following website contains strong language which may be offensive to some people and inappropriate for children. You have been warned!</SDModalContent>
+                    </SDModalContentContainer>
+                </Modal.Content>
+                <Modal.Actions>
+                    <SDModalButtonsContainer>
+                        <Button className="closeButton" onClick={() => leavePage()}>
+                            I would rather see some puppies
+                        </Button>
+                        <Button className="proceedButton" onClick={e => onClose(e)}>
+                            Proceed
+                        </Button>
+                    </SDModalButtonsContainer>
+                </Modal.Actions>
+            </SDModal>
         </Modal>
     );
 }
+
+const SDModal = styled.div`
+    background-color: #040430;
+    padding: 50px;
+    @media (max-width: 600px) {
+        padding: 30px 10px;
+    }
+`;
 
 const SDModalContentContainer = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
     padding: 30px;
+    background-color: #040430;
+    @media (max-width: 600px) {
+        padding: 0 0 30px 0;
+    }
 `;
 
 const SDModalHeader = styled.div`
-    font-size: 2rem;
+    font-size: 2.5rem;
     color: #1fda02;
+    text-align: center;
+    line-height: 1.5;
+    @media (max-width: 600px) {
+        font-size: 1.8rem;
+    }
 `;
 
 const SDModalContent = styled.div`
@@ -51,12 +70,15 @@ const SDModalContent = styled.div`
     font-size: 1.2rem;
     text-align: center;
     line-height: 2;
-    letter-spacing: 2;
+    @media (max-width: 600px) {
+        font-size: 1rem;
+    }
 `;
 
-const SDModalContainer = styled.div`
+const SDModalButtonsContainer = styled.div`
     display: flex;
     justify-content: center;
+    background-color: #040430;
     .proceedButton {
         background-color: #1fda02;
         color: white;
@@ -64,6 +86,11 @@ const SDModalContainer = styled.div`
         &:hover {
             background-color: white;
             color: #1fda02;
+        }
+        @media (max-width: 600px) {
+            font-size: 1rem;
+            line-height: 1.5;
+            margin: 0;
         }
     }
     .closeButton {
@@ -73,6 +100,11 @@ const SDModalContainer = styled.div`
         &:hover {
             background-color: white;
             color: #b401b5;
+        }
+        @media (max-width: 600px) {
+            font-size: 1rem;
+            line-height: 1.5;
+            margin: 0 10px 0 0;
         }
     }
 `;
