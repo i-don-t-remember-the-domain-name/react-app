9f00cf8ec189593fb102f1fc88b42c93c8e386c1
Ivana Huckova <ivana.huckova@gmail.com>
2019-06-16 16:41:11 +0200

    Add scroll to top of the window effect for each screen

--- src/screens/HNStatsPage.js
+++ src/screens/HNStatsPage.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 //Import components
 import HNStats from '../Components/HNStats/HNStats';
 
 //Export default component
 export default function HNStatsPage(props) {
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, []);
     return (
         <div>
             <HNStats theme={props.theme} />
